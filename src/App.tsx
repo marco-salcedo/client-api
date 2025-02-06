5345dbe23d6e614481d5a2167bb8a232690f6794
marco <marco@salcedo.io>
2025-02-05 21:04:22 -0800

    chore: creates project and adds deps

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen min-w-screen items-center justify-center">
+      <p className='text-center'>Postman Code Challenge</p>
+    </div>
+  )
+};
